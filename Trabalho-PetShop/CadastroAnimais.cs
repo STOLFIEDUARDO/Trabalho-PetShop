ea9bcbe568a4cafbd7708d76aff82be64397bcc6
STOLFIEDUARDO <38699786+STOLFIEDUARDO@users.noreply.github.com>
2018-05-29 16:16:19 -0300

    correção na interface

--- Trabalho-PetShop/CadastroAnimais.cs
+++ Trabalho-PetShop/CadastroAnimais.cs
@@ -16,5 +16,10 @@ namespace Trabalho_PetShop
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
--- Trabalho-PetShop/Form1.Designer.cs
+++ Trabalho-PetShop/Form1.Designer.cs
@@ -37,6 +37,14 @@
             this.btCadastrar6 = new System.Windows.Forms.Button();
             this.btCadastrar7 = new System.Windows.Forms.Button();
             this.btCadastrar8 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btCadastrar1
@@ -81,7 +89,7 @@
             // 
             // btCadastrar5
             // 
-            this.btCadastrar5.Location = new System.Drawing.Point(518, 44);
+            this.btCadastrar5.Location = new System.Drawing.Point(534, 44);
             this.btCadastrar5.Name = "btCadastrar5";
             this.btCadastrar5.Size = new System.Drawing.Size(94, 37);
             this.btCadastrar5.TabIndex = 4;
@@ -91,7 +99,7 @@
             // 
             // btCadastrar6
             // 
-            this.btCadastrar6.Location = new System.Drawing.Point(518, 134);
+            this.btCadastrar6.Location = new System.Drawing.Point(534, 134);
             this.btCadastrar6.Name = "btCadastrar6";
             this.btCadastrar6.Size = new System.Drawing.Size(94, 37);
             this.btCadastrar6.TabIndex = 5;
@@ -101,7 +109,7 @@
             // 
             // btCadastrar7
             // 
-            this.btCadastrar7.Location = new System.Drawing.Point(664, 44);
+            this.btCadastrar7.Location = new System.Drawing.Point(727, 44);
             this.btCadastrar7.Name = "btCadastrar7";
             this.btCadastrar7.Size = new System.Drawing.Size(94, 37);
             this.btCadastrar7.TabIndex = 6;
@@ -111,7 +119,7 @@
             // 
             // btCadastrar8
             // 
-            this.btCadastrar8.Location = new System.Drawing.Point(664, 134);
+            this.btCadastrar8.Location = new System.Drawing.Point(727, 134);
             this.btCadastrar8.Name = "btCadastrar8";
             this.btCadastrar8.Size = new System.Drawing.Size(94, 37);
             this.btCadastrar8.TabIndex = 7;
@@ -119,12 +127,100 @@
             this.btCadastrar8.UseVisualStyleBackColor = true;
             this.btCadastrar8.Click += new System.EventHandler(this.btCadastrar8_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("News706 BT", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(135, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 15);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Animais Banho e Tosa";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("News706 BT", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(309, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(149, 15);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Cadastro Funcionarios";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("News706 BT", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(481, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(197, 15);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Agendamento para Veterinario";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("News706 BT", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(746, 26);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 15);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "ZooPet";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("News706 BT", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(135, 116);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(143, 15);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Cadastro de Produtos";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("News706 BT", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(321, 116);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(119, 15);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Cadastro Clientes";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("News706 BT", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(522, 116);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(118, 15);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Cadastro Animais";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("News706 BT", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(724, 116);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(97, 15);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Adotar um Pet";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(925, 522);
+            this.ClientSize = new System.Drawing.Size(1014, 592);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btCadastrar8);
             this.Controls.Add(this.btCadastrar7);
             this.Controls.Add(this.btCadastrar6);
@@ -136,6 +232,7 @@
             this.Name = "Form1";
             this.Text = "MenuPrincipal";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -149,5 +246,13 @@
         private System.Windows.Forms.Button btCadastrar6;
         private System.Windows.Forms.Button btCadastrar7;
         private System.Windows.Forms.Button btCadastrar8;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
     }
 }
